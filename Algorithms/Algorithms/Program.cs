055a20e7af790c82c8ffc09bf737e1fd90ea734c
Pragramer <hideMisteriousMrProgrammer@gmail.com>
2019-03-09 00:01:37 +0300

    Adding bubble sorting

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,54 @@
+﻿using Sortings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Work();
+            Console.Read();
+        }
+
+        static void Work()
+        {
+            var countOfArrays = 10;
+            var countOfElements = 10000;
+            List<int[]> list = new List<int[]>();
+            for (var iteration = 0; iteration < countOfArrays; iteration++)
+            {
+                list.Add(CreateArray(countOfElements));
+            }
+            MeasureActionTime(list, (a) =>
+            {
+                a.BubbleSort();
+            });
+        }
+
+        static int[] CreateArray(int countOfElements)
+        {
+            var array = new int[countOfElements];
+            var random = new Random();
+            for (var position = 0; position < countOfElements; position++)
+            {
+                array[position] = random.Next(int.MinValue, int.MaxValue);
+            }
+            return array;
+        }
+
+        static void MeasureActionTime(List<int[]> arrays, Action<int[]> action)
+        {
+            var sw = new Stopwatch();
+            Console.WriteLine("Begin measure..");
+            sw.Start();
+
+            arrays.ForEach(action);
+
+            sw.Stop();
+            Console.WriteLine($"The end of action. Time of execution: {sw.ElapsedMilliseconds / 1000.0} sec.");
+        }
+    }
+}
---
+++ Algorithms/Sortings/ExchangeSorting.cs
@@ -0,0 +1,24 @@
+﻿namespace Sortings
+{
+    public static class ExchangeSorting
+    {
+        public static void BubbleSort(this int[] array)
+        {
+            var iterationCount = array.Length - 1;
+            for (var iteration = 0; iteration < iterationCount; iteration++)
+            {
+                var lastPosition = iterationCount - iteration;
+                for (var position = 0; position < lastPosition; position++)
+                {
+                    var firstСomparedElement = array[position];
+                    var secondCompareElement = array[position + 1];
+                    if (firstСomparedElement > secondCompareElement)
+                    {
+                        array[position] = secondCompareElement;
+                        array[position + 1] = firstСomparedElement;
+                    }
+                }
+            }
+        }
+    }
+}
